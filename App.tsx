0f8a4032e5ed7eb8d87418b858bb821481c758e1
Federicoverly <federicoverly@gmail.com>
2022-02-13 17:19:29 +0100

    first_commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
